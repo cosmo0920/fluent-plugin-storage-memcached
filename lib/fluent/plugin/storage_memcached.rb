cc0ddab74ba996f93c051ff4c7428af6404f8e0d
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2017-02-20 13:19:07 +0900

    Import

---
+++ lib/fluent/plugin/storage_memcached.rb
@@ -0,0 +1,125 @@
+require 'json'
+require 'dalli'
+require 'fluent/plugin/storage'
+
+module Fluent
+  module Plugin
+    class MemcachedStorage < Storage
+      Fluent::Plugin.register_storage('memcached', self)
+
+      config_param :path, :string, default: nil
+      config_param :host, :array, default: 'localhost'
+      config_param :port, :integer, default: 11211
+      config_param :namespace, :string, default: 'app_v1'
+      config_param :compress, :bool, default: true
+      config_param :username, :string, default: nil
+      config_param :password, :string, default: nil, secret: true
+      config_param :serializer, :enum, list: [:marshal, :json], default: :json
+      config_param :expires_in, :integer, default: 0
+      # Set persistent true by default
+      config_set_default :persistent, true
+
+      attr_reader :store # for test
+
+      def initialize
+        super
+
+        @store = {}
+      end
+
+      def configure(conf)
+        super
+
+        unless @path
+          if conf && !conf.arg.empty?
+            @path = conf.arg
+          else
+            raise Fluent::ConfigError, "path or conf.arg for <storage> is required."
+          end
+        end
+
+        serializer = case @serializer
+                     when :json
+                       JSON
+                     when :marshal
+                       Marshal
+                     end
+
+        options = {
+          thread_safe: true,
+          namespace: @namespace,
+          compress: @compress,
+          serializer: serializer,
+          expires_on: @expires_in,
+        }
+        options[:username] = @username if @username
+        options[:password] = @password if @password
+
+        @memcached = Dalli::Client.new("#{@host}:#{@port}", options)
+
+        object = @memcached.get(@path)
+        if object
+          begin
+            data = Yajl::Parser.parse(object)
+            raise Fluent::ConfigError, "Invalid contents (not object) in plugin redis storage: '#{@path}'" unless data.is_a?(Hash) unless data.is_a?(Hash)
+          rescue => e
+            log.error "failed to read data from plugin redis storage", path: @path, error: e
+            raise Fluent::ConfigError, "Unexpected error: failed to read data from plugin redis storage: '#{@path}'"
+          end
+        end
+      end
+
+      def multi_workers_ready?
+        true
+      end
+
+      def persistent_always?
+        true
+      end
+
+      def load
+        begin
+          json_string = @memcached.get(@path)
+          json = Yajl::Parser.parse(json_string)
+          unless json.is_a?(Hash)
+            log.error "broken content for plugin storage (Hash required: ignored)", type: json.class
+            log.debug "broken content", content: json_string
+            return
+          end
+          @store = json
+        rescue => e
+          log.error "failed to load data for plugin storage from memcached", path: @path, error: e
+        end
+      end
+
+      def save
+        begin
+          json_string = Yajl::Encoder.encode(@store)
+          @memcached.set(@path, json_string)
+        rescue => e
+          log.error "failed to save data for plugin storage to memcached", path: @path, error: e
+        end
+      end
+
+      def get(key)
+        @store[key.to_s]
+      end
+
+      def fetch(key, defval)
+        @store.fetch(key.to_s, defval)
+      end
+
+      def put(key, value)
+        @store[key.to_s] = value
+      end
+
+      def delete(key)
+        @store.delete(key.to_s)
+      end
+
+      def update(key, &block)
+        @store[key.to_s] = block.call(@store[key.to_s])
+      end
+    end
+  end
+end
---
+++ test/plugin/test_storage_memcached.rb
@@ -0,0 +1,129 @@
+require_relative '../helper'
+require 'fluent/test/helpers'
+require 'fluent/plugin/storage_memcached'
+require 'fluent/plugin/input'
+
+class MemcachedStorageTest < Test::Unit::TestCase
+  include Fluent::Test::Helpers
+
+  class MyInput < Fluent::Plugin::Input
+    helpers :storage
+    config_section :storage do
+      config_set_default :@type, 'memcached'
+    end
+  end
+
+  def setup_memcached
+    @store = {}
+    options = {
+      thread_safe: true,
+      namespace: "app_v1",
+      compress: true,
+      serializer: JSON,
+      expires_on: 0,
+    }
+    host = "localhost"
+    port = 11211
+
+    @memcached = Dalli::Client.new("#{host}:#{port}", options)
+  end
+
+  def teardown_memcached
+    @memcached.flush_all if @memcached
+  end
+
+  setup do
+    Fluent::Test.setup
+    @d = MyInput.new
+    setup_memcached
+    @path = 'my_store_key'
+  end
+
+  teardown do
+    @d.stop unless @d.stopped?
+    @d.before_shutdown unless @d.before_shutdown?
+    @d.shutdown unless @d.shutdown?
+    @d.after_shutdown unless @d.after_shutdown?
+    @d.close unless @d.closed?
+    @d.terminate unless @d.terminated?
+    teardown_memcached
+  end
+
+
+  sub_test_case 'without any configuration' do
+    test 'raise Fluent::ConfigError' do
+      conf = config_element()
+
+      assert_raise(Fluent::ConfigError) do
+        @d.configure(conf)
+      end
+    end
+  end
+
+  sub_test_case 'configured with path key' do
+    test 'works as storage which stores data into redis' do
+      storage_path = @path
+      conf = config_element('ROOT', '', {}, [config_element('storage', '', {'path' => storage_path})])
+      @d.configure(conf)
+      @d.start
+      @p = @d.storage_create()
+      assert_true(@p.persistent)
+
+      assert_equal storage_path, @p.path
+      assert @p.store.empty?
+
+      assert_nil @p.get('key1')
+      assert_equal 'EMPTY', @p.fetch('key1', 'EMPTY')
+
+      @p.put('key1', '1')
+      assert_equal '1', @p.get('key1')
+
+      @p.update('key1') do |v|
+        (v.to_i * 2).to_s
+      end
+      assert_equal '2', @p.get('key1')
+
+      @p.save # stores all data into redis
+
+      assert @p.load
+
+      @p.put('key2', 4)
+
+      @d.stop; @d.before_shutdown; @d.shutdown; @d.after_shutdown; @d.close; @d.terminate
+
+      assert_equal({'key1' => '2', 'key2' => 4}, @p.load)
+
+      # re-create to reload storage contents
+      @d = MyInput.new
+      @d.configure(conf)
+      @d.start
+      @p = @d.storage_create()
+
+      assert_false @p.store.empty?
+
+      assert_equal '2', @p.get('key1')
+      assert_equal 4, @p.get('key2')
+    end
+  end
+
+  sub_test_case 'configured with conf.arg' do
+    test 'works with customized path key by specified usage' do
+      storage_conf = {}
+      conf = config_element('ROOT', '', {}, [config_element('storage', "#{@path}", storage_conf)])
+      @d.configure(conf)
+      @d.start
+      @p = @d.storage_create(usage: "#{@path}")
+      assert_true(@p.persistent)
+
+      assert_equal @path, @p.path
+      assert @p.store.empty?
+
+      @p.put('key1', '1')
+      assert_equal '1', @p.get('key1')
+
+      @p.save # stores all data into redis
+
+      assert_equal({"key1"=>"1"}, @p.load)
+    end
+  end
+end
